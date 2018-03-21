feaf4838b76cb60914db821e004ad0f982e0b124
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-03-21 09:11:22 -0500

    suma

---
+++ lab/mate.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x=5;
+   int y=6;
+   int z=x+y;
+   int b=x*y;
+   int c=x-y;
+   int h=x/y
+   cout<<" el primer numero es "<< x<< "el segundo numero es "<<y<<endl;
+   cout<<" la multiplicacion es"<< b<<endl;
+   cout<<" la resta es"<< c<<endl;
+   cout<<" la divisiones"<<h<<endl;
+
+   return 0;
+}
